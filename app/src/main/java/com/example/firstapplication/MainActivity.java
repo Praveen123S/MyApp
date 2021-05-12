5c6edce22077d699c1f3ea039184b3c01b1785b3
visupraveen <visupraveen123@gmail.com>
2021-05-13 00:20:02 +0530

    first application

--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.firstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView image=findViewById(R.id.image);
+        image.setImageBitmap(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher_round));
     }
 }
